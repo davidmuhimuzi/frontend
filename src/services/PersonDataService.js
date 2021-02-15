dca3ae077ebd4eb609ed53b3dc33d4e576368b5b
Madi_Slocum <madisonslocum@sbcglobal.net>
2021-02-14 20:43:18 -0600

    Person Data Service

---
+++ src/services/PersonDataService.js
@@ -0,0 +1,26 @@
+//import http from "../http-common";
+
+class PersonDataService {
+
+getAll() {
+    return http.get("/person")
+}
+
+get(per_ID) {
+    return http.get(`person/${per_ID}`);
+}
+
+create(data) {
+    return http.post("person/", data);
+}
+
+update(per_ID, data) {
+    return http.put('person/${per_ID}');
+}
+
+}
+
+
+
+
+export default new PersonDataService();
