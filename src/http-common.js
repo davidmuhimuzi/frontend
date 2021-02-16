f56af25d038b57cae66c5435eb1a4fc066ba6480
Madi_Slocum <madisonslocum@sbcglobal.net>
2021-02-16 10:11:47 -0600

    updated node_env and http-common

---
+++ src/http-common.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+var baseurl;
+
+
+if (process.env.NODE_ENV === "development") {
+  baseurl = "http://localhost:8080/api/";
+} else {
+  baseurl = "/api/";
+}
+
+export default axios.create({
+    baseURL: baseurl,
+    headers: {
+        Accept: "application/json", 
+    "Content-type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    crossDomain: true
+  }
+});
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  devServer: {
+    host: 'localhost',
+  },
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/congregation/',
   transpileDependencies: ["vuetify"]
 };
