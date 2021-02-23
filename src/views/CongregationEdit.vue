ae4b6a3a9cc47a243e8a9f60a6ad1947239701b0
srathbun13 <shauna.rathbun@eagles.oc.edu>
2021-02-23 13:17:38 -0600

    Updated routing for pages

--- src/views/CongregationEdit.vue
+++ src/views/CongregationEdit.vue
@@ -33,7 +33,7 @@ export default {
     data(){
         return{
             currentCongregation: null,
-            message: 'Why null?'
+            message: ''
         };
     },
     methods: {
